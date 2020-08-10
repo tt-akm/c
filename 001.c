ef9af6802a1846ef7f939ab210ed41dba0041ce8
tt-akm <17633538563@163.com>
2020-08-10 21:27:18 +0800

    十进制转十六进制

---
+++ 001.c
@@ -0,0 +1,12 @@
+/* 十进制转十六进制 */
+
+#include <stdio.h>
+
+int main(){
+	int i;
+	printf("Please input decimalism number:\t");
+	scanf("%d", &i);
+	printf("The hex number is: %x\n", i);
+
+	return 0;
+}
